543694a4282185a0121a5ee921865142dab8542e
Grepsaw <gro.thehacker@gmail.com>
2025-07-03 11:45:33 +0200

    he agregado la division

--- prueba1.ts
+++ prueba1.ts
@@ -1,5 +1,6 @@
 let i = 3;
 let j = 3;
 
-console.log("resultado = ", i + j)
+console.log("resultado = ", i + j);
 console.log("resultado multiplicación =", i * j);
+console.log("la división es: ", i / j);
