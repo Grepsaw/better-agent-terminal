71fd9b87998f06b9e4d5fce97c752604607d3520
nrodriguezlv <85559654+nrodriguezlv@users.noreply.github.com>
2025-07-03 11:51:37 +0200

    Hacemos los cambios del cliente

--- prueba1.ts
+++ prueba1.ts
@@ -1,6 +1,7 @@
-let i = 3;
+let i = 4;
 let j = 3;
 
 console.log("resultado = ", i + j);
 console.log("resultado multiplicación =", i * j);
 console.log("la división es: ", i / j);
+console.log("la resta es: ", i - j);
